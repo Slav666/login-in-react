a66681cb8cfcae72fbb077086d2082bd6666219f
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-11-02 22:50:47 +0000

    work on unit tests and zod validation

--- src/utility/interface.tsx
+++ src/utility/interface.tsx
@@ -1,11 +1,25 @@
-export interface IPost {
-  userId(arg0: string, userId: any): unknown;
-  id: number;
-  ownerId: number;
-  creationDate: string;
-  title: string;
-  post: string;
-}
-export interface IUser {
-  userId: number;
-}
+import { z } from 'zod';
+
+export const BlogData = z.object({
+  id: z.number(),
+  ownerId: z.number(),
+  creationDate: z.string(),
+  title: z.string(),
+  post: z.string(),
+});
+
+export const IUser = z.object({
+  userId: z.number(),
+});
+
+// export interface IPost {
+//   userId(userId: number);
+//   id: number;
+//   ownerId: number;
+//   creationDate: string;
+//   title: string;
+//   post: string;
+// }
+// export interface IUser {
+//   userId: number;
+// }
